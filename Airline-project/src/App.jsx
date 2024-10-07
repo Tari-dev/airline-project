b6138cd2f389ebb0174e0543bff52b0ab97f8a73
MAHESHPPAI <maheshpai200424@gmail.com>
2024-10-07 18:31:18 +0530

    Added services page

---
+++ Airline-project/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+const App=()=>{
+  return(
+    <div>
+    <body className="h-screen bg-green-300">
+    <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos architecto vel et delectus quibusdam atque harum quo fugiat temporibus voluptatem ipsam qui enim, eum iste. Aliquid maxime sed iste quidem.</p>
+    </body>
+    </div>
+  );
+  };
+ export default App;
