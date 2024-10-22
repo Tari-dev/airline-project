39bf0e17a77474533789a8a79fd96ca0fbe54878
MAHESHPPAI <maheshpai200424@gmail.com>
2024-10-22 23:31:06 +0530

    jwfhhvev

--- app.py
+++ app.py
@@ -1,58 +1,10 @@
-from flask import Flask,render_template,redirect,request
-import sqlite3 as sql
+from flask import Flask
+from index import index_app  # Import the Flask app (blueprint) from index.py
+from login import login_app  # Import the login blueprint from login.py
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.register_blueprint(index_app)  # Register the blueprint from index.py
+app.register_blueprint(login_app)  # Register the login blueprint from login.py
 
-
-conn=sql.connect('airline.db')
-c=conn.cursor()
-
-
-@app.route('/')
-def home():
-    return render_template('intropage.html')
-
-
-@app.route('/book_a_flight',methods=['GET','POST'])
-def bood_a_flight():
-    if request=='POST':
-        From=request.form['from']
-        To=request.form['to']
-        Passengers=request.form['passenger']
-        q='SELECT * FROM TABLE_NAME WHERE FROM=? AND TO=? AND PASSENGERS=?' #change the table name
-        c.execute(q,(From,To,Passengers))
-        table=c.fetchall()
-        return render_template('intropage',tables=table)
-    return render_template('intropage')
-
-
-@app.route('/manage',methods=['GET','POST'])
-def manage():
-    if request=='POST':
-        refno=request.form['reference_number']
-        lastname=request.form['last_name']   #also we need to add the username of the user so i will modify later
-        q='DELETE TABLE_NAME WHERE REFERENCE_NUMBER=?'
-        c.execute(q,refno)
-        val=c.fetchall()
-        if len(val)==0:
-            return redirect('No_Payment_Has_Done.html') #go to the page showing the you havnt done the payment
-        return redirect('message.html') #message showing flight has been cancelled
-    return render_template('intropage')
-
-
-@app.route('/flight_status',methods=['GET','POST'])
-def flight_status():
-    if request=='POST':
-        From=request.form['from']
-        To=request.form['to']
-        date=request.form['date']
-        q='SELECT * FROM TABLE_NAME WHERE FROM=? AND TO=?' #change the table name
-        c.execute(q,(From,To))
-        val=c.fetchall()
-        return render_template('intropage',table=val)
-    return render_template('intropage')
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
-
--- index.py
+++ index.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, redirect, request
+from flask import Blueprint, render_template, request
 import sqlite3 as sql
+
+# Blueprint setup
+index_app = Blueprint('index', __name__)
+
+# Dictionary for airport codes
 codes = {
     'COK': 'Cochin International Airport',
     'CCJ': 'Calicut International Airport',
@@ -16,14 +21,12 @@ codes = {
     'LHR': 'London Heathrow Airport',
     'JFK': 'John F. Kennedy International Airport'
 }
-app = Flask(__name__)
 
-@app.route('/')
+@index_app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html')  # Load the index.html file
 
-
-@app.route('/book_a_flight', methods=['GET', 'POST'])
+@index_app.route('/book_a_flight', methods=['GET', 'POST'])
 def book_a_flight():
     if request.method == 'POST':
         From = request.form['from']
@@ -31,32 +34,30 @@ def book_a_flight():
         Passengers = request.form['passenger']
         conn = sql.connect('airline.db')
         c = conn.cursor()
-        q = 'SELECT DEPARTUREAIRPORT, ARRIVALAIRPORT, AircraftType, Price FROM FLIGHT WHERE DEPARTUREAIRPORT LIKE ? AND ARRIVALAIRPORT LIKE ?'
+        q = 'SELECT DEPARTUREAIRPORT, ARRIVALAIRPORT, AircraftType, Price FROM Flight WHERE DEPARTUREAIRPORT LIKE ? AND ARRIVALAIRPORT LIKE ?'
         c.execute(q, (From, To))
         table = c.fetchall()
         conn.close()
-        departf=codes[From]
-        arrivalf=codes[To]
-        return render_template('bookingpage.html', tables=table,From=departf,To=arrivalf)
+        departf = codes[From]
+        arrivalf = codes[To]
+        return render_template('bookingpage.html', tables=table, From=departf, To=arrivalf)
     return render_template('index.html')
 
-
-@app.route('/manage', methods=['GET', 'POST'])
+@index_app.route('/manage', methods=['GET', 'POST'])
 def manage():
     if request.method == 'POST':
         refno = request.form['reference_number']
-        lastname = request.form['last_name']  
+        lastname = request.form['last_name']
         conn = sql.connect('airline.db')
         c = conn.cursor()
-        q = 'DELETE FROM TABLE_NAME WHERE REFERENCE_NUMBER=?'
+        q = 'DELETE FROM FLIGHT WHERE REFERENCE_NUMBER=?'  # Replace with the correct table name
         c.execute(q, (refno,))
         conn.commit()
         conn.close()
-        return redirect('message.html') 
+        return render_template('message.html')  # Redirect to a confirmation page
     return render_template('intropage.html')
 
-
-@app.route('/flight_status', methods=['GET', 'POST'])
+@index_app.route('/flight_status', methods=['GET', 'POST'])
 def flight_status():
     if request.method == 'POST':
         From = request.form['from']
@@ -64,13 +65,9 @@ def flight_status():
         date = request.form['date']
         conn = sql.connect('airline.db')
         c = conn.cursor()
-        q = 'SELECT * FROM TABLE_NAME WHERE FROM=? AND TO=?'  # Change the table name
+        q = 'SELECT * FROM FLIGHT WHERE FROM=? AND TO=?'  # Change table name
         c.execute(q, (From, To))
         val = c.fetchall()
         conn.close()
-        return render_template('intropage.html', table=val)
+        return render_template('flightstatus.html', table=val)  # Add a status display page
     return render_template('intropage.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
