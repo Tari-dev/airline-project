e7482a0d2cbce264e2b7d55a37008de20950aad3
MAHESHPPAI <maheshpai200424@gmail.com>
2024-10-27 21:32:02 +0530

    Modified bookingpage, passenger, services, init.py, login.py, passenger.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,12 +1,18 @@
+import os
 from flask import Flask
 from flaskr.index import index_app
 from flaskr.login import login_app
+from flaskr.booking import booking_app
+from flaskr.passenger import passenger_app
 
+BLUEPRINTS = [index_app, login_app, booking_app, passenger_app]
 
-BLUEPRINTS = [index_app, login_app]
 
 def create_app():
     app = Flask(__name__)
+
+    # Set a secret key for session management
+    app.secret_key = os.environ.get('SECRET_KEY', 'a_default_secret_key')
 
     for bp in BLUEPRINTS:
         app.register_blueprint(bp)
@@ -20,3 +26,4 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         app.db.close()
         print('Exiting...')
+
